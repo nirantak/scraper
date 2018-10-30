12b18d711d562970f84bc118c1703eb6809b5aec
Nirantak Raghav <me@nirantak.com>
2018-10-30 23:41:25 +0530

    Update packages

--- README.md
+++ README.md
@@ -1,34 +1,43 @@
 # Scraper
+
 > *A Python web scraper built using BS4 and Selenium*
 
 ### Table of Contents
+
 * [Installation](#installation)
 * [Usage](#usage)
 * [Requirements](#requirements)
 
 ### Installation
+
 Clone the git repository:
+
 ```bash
 $ git clone https://github.com/nirantak/scraper.git && cd scraper
 ```
 
 Install necessary dependencies
+
 ```bash
-$ pip install -r requirements.txt
+$ pipenv install --dev
 ```
 
 ### Usage
-Save Instagram username and password in file **config.py** as:
-```python
-USERNAME = "your_username_here"
-PASSWORD = "your_password_here"
+
+Save Instagram username and password in the **.env** file as:
+
+```shell
+USERNAME="your_username_here"
+PASSWORD="your_password_here"
 ```
+
 Run script
+
 ```bash
-$ python instagram.py
+$ pipenv run python instagram.py
 ```
 
 ### Requirements
+
 1. [Python](https://python.org)
 2. [ChromeDriver](https://sites.google.com/a/chromium.org/chromedriver/)
-
--- instagram.py
+++ instagram.py
@@ -1,52 +1,55 @@
+import os
+from typing import List
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
-import config
-username = config.USERNAME
-password = config.PASSWORD
-
-def login(driver):
-	# Load page
-	driver.get("https://www.instagram.com/accounts/login/")
-
-	# Login
-	driver.find_element_by_xpath("//div/input[@name='username']").send_keys(username)
-	driver.find_element_by_xpath("//div/input[@name='password']").send_keys(password)
-	driver.find_element_by_xpath("//span/button").click()
-
-	# Wait for the login page to load
-	WebDriverWait(driver, 30).until(
-		EC.presence_of_element_located((By.LINK_TEXT, "Profile")))
+from selenium.webdriver.support.ui import WebDriverWait
 
 
-def scrape_followers(driver, account):
-	# Load account page
-	driver.get("https://www.instagram.com/{0}/".format(account))
+def login(driver: webdriver, username: str, password: str):
+    # Load page
+    driver.get("https://www.instagram.com/accounts/login/")
 
-	# Click the 'Follower(s)' link
-	driver.find_element_by_partial_link_text("follower").click()
+    # Login
+    driver.find_element_by_xpath("//input[@name='username']").send_keys(username)
+    driver.find_element_by_xpath("//input[@name='password']").send_keys(password)
+    driver.find_element_by_xpath("//button[@type='submit']").click()
 
-	# Wait for the followers modal to load
-	xpath = "//div[@style='position: relative; z-index: 1;']/div/div[2]/div/div[1]"
-	WebDriverWait(driver, 10).until(
-		EC.presence_of_element_located((By.XPATH, xpath)))
+    # Wait for 2FA or Profile link to appear
+    WebDriverWait(driver, 30).until(
+        EC.presence_of_element_located((By.LINK_TEXT, "Profile"))
+    )
 
-	# Pending Scrolling Magic Here #
 
-	# Scrape the followers
-	xpath = "//div[@style='position: relative; z-index: 1;']//ul/li/div/div/div/div/a"
-	followers_elems = driver.find_elements_by_xpath(xpath)
+def scrape_followers(driver: webdriver, username: str) -> List[str]:
+    # Load account page
+    driver.get(f"https://www.instagram.com/{username}/")
 
-	return [e.text for e in followers_elems]
+    # Click the 'Followers' link
+    driver.find_element_by_partial_link_text("follower").click()
+
+    # Wait for the followers modal to load
+    xpath = "//div[@style='position: relative; z-index: 1;']/div/div[2]/div/div[1]"
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, xpath)))
+
+    # TODO: Scrolling Magic Here #
+
+    # Scrape the followers
+    xpath = "//div[@style='position: relative; z-index: 1;']//ul/li/div/div/div/div/a"
+    followers_elems = driver.find_elements_by_xpath(xpath)
+
+    return [e.text for e in followers_elems]
 
 
 if __name__ == "__main__":
-	driver = webdriver.Chrome('drivers/chromedriver_win32.exe')
-	try:
-		login(driver)
-		followers = scrape_followers(driver, username)
-		print(followers)
-	finally:
-		driver.quit()
+    driver = webdriver.Chrome("drivers/chromedriver_linux64")
+    user = os.environ["USERNAME"]
+    passwd = os.environ["PASSWORD"]
+
+    try:
+        login(driver, user, passwd)
+        followers = scrape_followers(driver, user)
+        print(followers)
+    finally:
+        driver.quit()
